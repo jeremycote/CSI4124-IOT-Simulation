71668863a6b8ce12aabd7a3110de8d17d0b0dcd0
Jeremy <jeremymcote@gmail.com>
2023-11-08 19:23:45 -0500

    Switch to milliseconds

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from abc import ABC, abstractmethod
 from enum import Enum
-from typing import List
+from typing import List, Optional
 
 from message import Message
+
+import numpy as np
 
 
 class OnOffServerState(Enum):
@@ -16,18 +18,21 @@ class SimulationComponent(ABC):
     """Protocol for objects that are orchastrated by the simulation"""
 
     @abstractmethod
-    def tick(time: int):
+    def tick(self, time: int):
         """Method called every tick of the simulation"""
 
 
 class Simulation:
-    time: int
+    time: int = 0
     components: List[SimulationComponent] = []
+
+    def __init__(self) -> None:
+        pass
 
     def register_component(self, component: SimulationComponent):
         self.components.append(component)
 
-    def simulate(self, start_time: int = 0, end_time: int = 10):
+    def simulate(self, start_time: int = 0, end_time: int = 10000):
         for i in range(start_time, end_time):
             for component in self.components:
                 component.tick(i)
@@ -37,21 +42,24 @@ class Input(ABC):
     """Protocol for objects that can take the output of other objects"""
 
     @abstractmethod
-    def process_input(message: Message) -> bool:
+    def process_input(self, message: Message, time: int) -> bool:
         """Returns True if the input is accepeted"""
 
 
 class Output(ABC):
     """Protocol for objects that can output"""
 
-    listeners: List[Input] = []
+    listeners: List[Input]
+
+    def __init__(self) -> None:
+        self.listeners = []
 
     def register_output_listener(self, destination):
         self.listeners.append(destination)
 
-    def output(self, message: Message):
+    def output(self, message: Message, time: int):
         for listener in self.listeners:
-            message_accepted = listener.process_input(message)
+            message_accepted = listener.process_input(message, time)
 
             if message_accepted:
                 return
@@ -68,36 +76,110 @@ class Generator(Output, SimulationComponent):
 class Sink(Input, SimulationComponent):
     """"""
 
-    def process_input(self, message: Message) -> bool:
-        return super().process_input()
+    def process_input(self, message: Message, time: int) -> bool:
+        return super().process_input(message, time)
 
     def tick(self, time: int):
-        return super().tick()
+        return super().tick(time)
 
 
 class MessageGenerator(Generator):
     """Generator for IOT Messages"""
 
+    # Time at which to generate the next message
+    next_generation: int = 0
+
+    # Lambda value for poisson distribution
+    l: float
+
+    # Create message generator with lambda parameter
+    def __init__(self, l: float) -> None:
+        super().__init__()
+        self.l = l
+
     def tick(self, time: int):
-        self.output(Message())
+        if self.next_generation <= time:
+            # Round the inter arrival time to the nearest millisecond
+            inter_arrival_time = round(np.random.exponential(scale=1 / self.l) * 1000)
+            self.next_generation += inter_arrival_time
+
+            # Output the message from the generator
+            self.output(Message(arrival_time=time), time)
 
 
 class OnOffServer(Server):
     """A server that can be ON or OFF"""
 
-    state: OnOffServerState
+    # The lambda parameter to generate service times
+    l: float
 
-    def process_input(self, message: Message) -> bool:
-        print(message)
-        return super().process_input()
+    # Is the server ON or OFF
+    state: OnOffServerState = OnOffServerState.ON
+
+    # The message that the server is currently processing
+    message: Optional[Message] = None
+
+    # How long it will take to process the message
+    message_service_length: Optional[int] = None
+
+    # When did message processing start
+    message_service_start: Optional[int] = None
+
+    def __init__(self, l: float) -> None:
+        """Construct an OnOffServer
+
+        Args:
+            l (float): Lambda parameter for generating service times
+        """
+        super().__init__()
+        self.l = l
+
+    def output_message(self, time: int):
+        if self.message is None:
+            raise ValueError("No message to output")
+
+        self.message.work_time += self.message_service_length
+
+        print(f"Departing {self.message}")
+
+        self.output(self.message, time)
+        self.message = None
+
+    def process_input(self, message: Message, time: int) -> bool:
+        # Do not accept messages if the server is off
+        if self.state is OnOffServerState.OFF:
+            return False
+
+        if (
+            self.message
+            and time <= self.message_service_start + self.message_service_length
+        ):
+            # Message is complete
+            self.output_message(time)
+        elif self.message:
+            return False
+
+        self.message = message
+        self.message_service_start = time
+
+        # Generate a message service length rounded to the nearest millisecond
+        self.message_service_length = round(np.random.exponential(self.l) * 1000)
+
+        print(self.message)
+        return True
 
     def tick(self, time: int):
-        return super().tick()
+        if (
+            self.message
+            and time <= self.message_service_start + self.message_service_length
+        ):
+            # Message is complete
+            self.output_message(time)
 
 
 if __name__ == "__main__":
-    message_generator = MessageGenerator()
-    server = OnOffServer()
+    message_generator = MessageGenerator(l=1)
+    server = OnOffServer(l=1)
     sink = Sink()
 
     message_generator.register_output_listener(server)
@@ -108,7 +190,8 @@ if __name__ == "__main__":
     simulation.register_component(server)
     simulation.register_component(sink)
 
-    simulation.simulate(0, 10)
+    # Simulate for 10000ms -> 10s
+    simulation.simulate(0, 10000)
 
 # class QueueItem(Protocol):
 #     def start_service(self, time: int):
--- message.py
+++ message.py
@@ -1,6 +1,19 @@
 from dataclasses import dataclass
+from typing import Optional
 
 
 @dataclass
 class Message:
     """Represents a message being passed between nodes in the simulation"""
+
+    # Absolute arrival time
+    arrival_time: int
+
+    # Absolute departure time
+    departure_time: Optional[int] = None
+
+    # How long has the message waited (Cumulative)
+    wait_time: int = 0
+
+    # How long has the message been worked on (Cumulative)
+    work_time: int = 0
